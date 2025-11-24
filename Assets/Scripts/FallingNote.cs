6e34397a3083ffdee7383d2a7d13452f30234ccb
Suryaveer Bhandari <suryaveerb7@gmail.com>
2025-11-24 16:27:47 +0530

    Update FallingNote.cs

--- Assets/Scripts/FallingNote.cs
+++ Assets/Scripts/FallingNote.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class FallingNote : MonoBehaviour
 {
-    public int noteID; // 0=Red, 1=Blue, etc.
+    public int noteID; 
     public float fallSpeed = 10f;
     public float spinSpeed = 100f;
 
@@ -14,7 +14,7 @@ public class FallingNote : MonoBehaviour
         transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
         transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime, Space.World);
 
-        // Safety destroy if it falls way below map (just in case)
+       
         if (transform.position.y < -10f) Destroy(gameObject);
     }
 
@@ -22,18 +22,18 @@ public class FallingNote : MonoBehaviour
     {
         if (hasTriggered) return;
 
-        // 1. CAUGHT BY PLAYER (GOOD)
+        
         if (other.CompareTag("Player"))
         {
             hasTriggered = true;
-            MusicManager.Instance.NoteCaught(noteID); // Play Sound
+            MusicManager.Instance.NoteCaught(noteID); 
             Destroy(gameObject);
         }
-        // 2. HIT THE FLOOR (BAD)
+       
         else if (other.CompareTag("Finish"))
         {
             hasTriggered = true;
-            MusicManager.Instance.NoteMissed(); // GAME OVER!
+            MusicManager.Instance.NoteMissed(); 
             Destroy(gameObject);
         }
     }
